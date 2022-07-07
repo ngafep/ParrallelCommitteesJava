0a8220bc6c01ae6bee20fb2d6e3e27ee5699eca9
Thibaut Rety <trety@positivethinking.tech>
2022-07-07 10:11:40 +0200

    Clean

--- core/src/main/java/com/engie/csai/pc/core/model/RecvRabbitMQ.java
+++ core/src/main/java/com/engie/csai/pc/core/model/RecvRabbitMQ.java
@@ -36,7 +36,7 @@ public class RecvRabbitMQ {
         throws IOException, TimeoutException {
         boolean autoAck = false;
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost("rabbitmq");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
         channel.queueDeclare(queueName, false, false, false, null);
